1e451fc74dbf431988020fb0dceff477f4b6dd3f
Ricardo Ghaoui <202310112@ua.edu.lb>
2026-07-30 09:18:30 +0300

    Initial Blazor Web App with Identity template.

---
+++ time-off-management-app/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,8 @@
+namespace time_off_management_app.Components.Account
+{
+    public class PasskeyInputModel
+    {
+        public string? CredentialJson { get; set; }
+        public string? Error { get; set; }
+    }
+}
---
+++ time-off-management-app/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,8 @@
+namespace time_off_management_app.Components.Account
+{
+    public enum PasskeyOperation
+    {
+        Create = 0,
+        Request = 1,
+    }
+}
